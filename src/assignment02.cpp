603ea440eead117c5d4fd998c88990a78f93fe65
rylanweber <rylandoyle@mail.weber.edu>
2025-09-06 02:22:51 +0000

    added comments

--- src/assignment02.cpp
+++ src/assignment02.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <iomanip>
-#include <limits>
+#include <limits> // allows for infinite value
 
 using namespace std;
 
 int main () {
+    // variables
     int integer1;
     int integer2;
     int integer3;
@@ -18,12 +19,16 @@ int main () {
 
     do {
         int recieved_input;
+        // input
         cout << "Enter a positive integer that is a multiple of 3: " << endl;
         cin >> recieved_input;
+        // check to make sure number is valid
         if (recieved_input <= 0 || recieved_input % 3 != 0) {
             cout << "Invalid integer: not a positive or a multiple of 3." << endl;
         } else {
+            // increase valid count
             total_valid += 1;
+            // assign input to integer for average calculations
             if (total_valid == 1) {
                 integer1 = recieved_input;
             } else if (total_valid == 2) {
@@ -35,7 +40,9 @@ int main () {
             } else if (total_valid == 5) {
                 integer5 = recieved_input;
             }
+            // increase total
             total += recieved_input;
+            // assign max and min
             if (recieved_input > maximum) {
                 maximum = recieved_input;
             } else if (recieved_input < minimum) {
@@ -44,8 +51,10 @@ int main () {
         }
     } while (total_valid < 5);
 
+    // calculate average
     average = (integer1 + integer2 + integer3 + integer4 + integer5) / 5.0;
 
+    // display
     cout << "Count" << setw(12) << "Minimum" << setw(12) << "Maximum" << setw(12) << "Total" << setw(12) << "Average" << endl;
     cout << "=======================================================" << endl;
     cout << setw(5) << total_valid << setw(12) << minimum << setw(12) << maximum << setw(12) << total << setw(12) << average << endl;
