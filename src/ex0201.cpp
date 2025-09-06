5a08e863269b8a9747c01a8069513ae90d29d160
rylanweber <rylandoyle@mail.weber.edu>
2025-09-06 00:31:20 +0000

    week 02 programs

---
+++ src/ex0201.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    cout << setw(6) << "Odd" << setw(6) << "Even" << endl;
+    // for(int i = 1; i < 20; i += 2) {
+    //     cout << setw(6) << i << setw(6) << i + 1 << endl;
+    // }
+    int i = 1;
+    while (i < 20) {
+        cout << setw(6) << i << setw(6) << i + 1 << endl;
+        i += 2;
+    }
+
+    return 0;
+}
